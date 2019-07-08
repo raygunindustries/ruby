1e5b7af3d9a2242a0e79bb3a1131f850e3ce165e
raygun.industries <raygunundone@gmail.com>
2019-07-08 14:15:31 -0400

    add refactoring of always_three project

--- ruby_challenges/always_three.rb
+++ ruby_challenges/always_three.rb
@@ -2,16 +2,6 @@
 puts "Give me a number"
 #Grab that number and transform it into an Integer (since it will start out as a String). HINT: use the gets method to grab the number.
 #Set the resulting number to a variable, let’s call it Number 1
-first_number = gets.to_i
-#Add 5 to Number 1
-mid_number = first_number + 5
-#Multiply the result by 2
-mid_number *=2
-#Subtract 4
-mid_number -=4
-#Divide by 2
-mid_number /=2
-#Subtract the first number from the final number
-final_number = mid_number - first_number
+number = gets.to_i
 #Tell us the final number
-puts "Always #{final_number}"
+puts 'Always ' + (((number + 5)*2-4)/2 - number).to_s
