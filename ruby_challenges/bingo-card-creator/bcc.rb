63c7f8cf42e78eff68284757e82d3bb35f10c3f9
raygun.industries <raygunundone@gmail.com>
2019-08-08 12:13:03 -0400

    add b,i,g,and o columns to pdf

--- ruby_challenges/bingo-card-creator/bcc.rb
+++ ruby_challenges/bingo-card-creator/bcc.rb
@@ -37,10 +37,41 @@ header = ["B", "I", "N", "G", "O"]
 Prawn::Document.generate("bingo.pdf") do
     define_grid(columns: 5, rows: 6)
 
-    header.each_with_index do |char, i|
+    header.each_with_index do |h, i|
         grid(0, i).bounding_box do
             stroke_bounds
-            text char, align: :center, valign: :center, size: 50, style: :bold
+            text h, align: :center, valign: :center, size: 50, style: :bold
         end
     end
+
+    b_col.each_with_index do |num, i|
+        grid((i + 1), 0).bounding_box do
+            stroke_bounds
+            text num.to_s, align: :center, valign: :center, size: 50
+        end
+    end
+
+    i_col.each_with_index do |num, i|
+        grid((i + 1), 1).bounding_box do
+            stroke_bounds
+            text num.to_s, align: :center, valign: :center, size: 50
+        end
+    end
+
+    g_col.each_with_index do |num, i|
+        grid((i + 1), 3).bounding_box do
+            stroke_bounds
+            text num.to_s, align: :center, valign: :center, size: 50
+        end
+    end
+
+    o_col.each_with_index do |num, i|
+        grid((i + 1), 4).bounding_box do
+            stroke_bounds
+            text num.to_s, align: :center, valign: :center, size: 50
+        end
+    end
+
+
+
 end
