8bf29f67b2ce5b20b7ad142f8b9711e5faa6ab14
raygun.industries <raygunundone@gmail.com>
2019-08-23 16:44:23 -0400

    add what_shoud_i_eat version 0.1.0.

--- ruby_challenges/what_should_i_eat/lib/what_should_i_eat.rb
+++ ruby_challenges/what_should_i_eat/lib/what_should_i_eat.rb
@@ -1,6 +1,15 @@
 require "what_should_i_eat/version"
+require 'what_should_i_eat/printing.rb'
+require 'net/http'
+require 'json'
 
 module WhatShouldIEat
   class Error < StandardError; end
-  # Your code goes here...
+  def WhatShouldIEat.fresh
+    result = Net::HTTP.get('www.edamam.com', '/search?type=Feeds')
+    recipes = JSON.parse(result)
+    name = recipes.first['items'].first['label']
+    url = recipes.first['items'].first['url']
+    Printing.print name, url
+  end
 end
---
+++ ruby_challenges/what_should_i_eat/lib/what_should_i_eat/printing.rb
@@ -0,0 +1,8 @@
+module WhatShouldIEat
+  module printing
+    def Printing.print(name, url)
+      puts name
+      puts url
+    end
+  end
+end
